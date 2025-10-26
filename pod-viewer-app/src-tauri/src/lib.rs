fd5ff6c10959b42c3b5152eeaec78d0d61a0f031
jrobertson627 <jessicarobertson627@gmail.com>
2025-10-26 12:07:36 -0700

    Adds in the get pods shell command

--- pod-viewer-app/src-tauri/src/lib.rs
+++ pod-viewer-app/src-tauri/src/lib.rs
@@ -1,14 +1,30 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+use tauri::command;
+use std::process::Command;
+
+#[command]
+fn get_pods() -> Result<String, String> {
+    let output = Command::new("minikube")
+    .args(["kubectl", "--", "get", "pods", "-A"])
+    .output()
+    .map_err(|e| e.to_string())?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).into_owned())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).into_owned())
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_pods])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
